1704ec2f33bf31a6379a4f24d063e753cd00f422
Chaozhu Qiang <chaozhuqiang@Chaozhus-MacBook-Air.local>
2025-02-02 23:16:40 -0800

    address conflict

---
+++ src/main/java/com/example/fetchtakehomeexercise/FetchTakeHomeExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.fetchtakehomeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchTakeHomeExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchTakeHomeExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fetchtakehomeexercise/FetchTakeHomeExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetchtakehomeexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchTakeHomeExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
